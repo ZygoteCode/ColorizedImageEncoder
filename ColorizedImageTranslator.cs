1668671b22cd4142e9fd6019f9b74456014aee72
Gabriele Bologna <bolognagabriele2003@gmail.com>
2022-08-30 22:40:20 +0200

    First commit

---
+++ ColorizedImageTranslator.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+public class ColorizedImageTranslator
+{
+    public static Bitmap StringConvertBytesToBitmap(byte[] data, int imageWidth, int imageHeight)
+    {
+        if (data == null || imageWidth <= 10 || imageHeight <= 10)
+        {
+            return null;
+        }
+
+        byte[] completeData = BitConverter.GetBytes(data.Length);
+
+        completeData = Combine(completeData, GetMD5(data));
+        completeData = Combine(completeData, new byte[1] { 89 });
+        completeData = Combine(completeData, data);
+        completeData = Combine(completeData, new byte[1] { 0xF8 });
+
+        int totalLength = completeData.Length;
+        Bitmap bitmap = new Bitmap(imageWidth, imageHeight);
+        int initialIndex = 0;
+
+        for (int i = 0; i < bitmap.Width; i++)
+        {
+            for (int j = 0; j < bitmap.Height; j++)
+            {
+                if (initialIndex >= totalLength)
+                {
+                    bitmap.SetPixel(i, j, Color.White);
+                }
+                else
+                {
+                    byte item = completeData[initialIndex];
+                    bitmap.SetPixel(i, j, Color.FromArgb(item, item, item));
+                    initialIndex++;
+
+                    if (initialIndex == totalLength)
+                    {
+                        bitmap.SetPixel(i, j, Color.FromArgb(45, 47, 50));
+                    }
+                }
+            }
+        }
+
+        return bitmap;
+    }
+
+    public static byte[] StringConvertBitmapToBytes(Bitmap bitmap)
+    {
+        try
+        {
+            if (bitmap.Width != 512 || bitmap.Height != 512)
+            {
+                return null;
+            }
+
+            byte[] completeData = null;
+            bool finish = false;
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                if (!finish)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        if (!finish)
+                        {
+                            if (bitmap.GetPixel(i, j).R == 45 && bitmap.GetPixel(i, j).G == 47 && bitmap.GetPixel(i, j).B == 50)
+                            {
+                                finish = true;
+                                break;
+                            }
+                            else
+                            {
+                                if (completeData == null)
+                                {
+                                    completeData = new byte[1] { bitmap.GetPixel(i, j).R };
+                                }
+                                else
+                                {
+                                    completeData = Combine(completeData, new byte[1] { bitmap.GetPixel(i, j).R });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            int dataSize = BitConverter.ToInt32(completeData.Take(4).ToArray(), 0);
+            completeData = completeData.Skip(4).ToArray();
+
+            byte[] md5Hash = completeData.Take(16).ToArray();
+            completeData = completeData.Skip(16).ToArray();
+
+            byte isFile = completeData.Take(1).ToArray()[0];
+            completeData = completeData.Skip(1).ToArray();
+
+            if (isFile != 89)
+            {
+                return null;
+            }
+
+            if (!CompareByteArrays(md5Hash, GetMD5(completeData)))
+            {
+                return null;
+            }
+
+            if (dataSize != completeData.Length)
+            {
+                return null;
+            }
+
+            return completeData;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static Bitmap FileConvertBytesToBitmap(byte[] data, string fileName, string fileExtension, int imageWidth, int imageHeight)
+    {
+        try
+        {
+            if (data == null || imageWidth <= 10 || imageHeight <= 10)
+            {
+                return null;
+            }
+
+            byte[] completeData = BitConverter.GetBytes(data.Length);
+
+            completeData = Combine(completeData, GetMD5(data));
+            completeData = Combine(completeData, new byte[1] { 54 });
+
+            byte[] fileNameBytes = Encoding.UTF8.GetBytes(fileName);
+            byte[] fileExtensionBytes = Encoding.UTF8.GetBytes(fileExtension);
+
+            completeData = Combine(completeData, BitConverter.GetBytes(fileNameBytes.Length));
+            completeData = Combine(completeData, fileNameBytes);
+
+            completeData = Combine(completeData, BitConverter.GetBytes(fileExtension.Length));
+            completeData = Combine(completeData, fileExtensionBytes);
+
+            completeData = Combine(completeData, data);
+            completeData = Combine(completeData, new byte[1] { 0xF8 });
+
+            int totalLength = completeData.Length;
+            Bitmap bitmap = new Bitmap(imageWidth, imageHeight);
+            int initialIndex = 0;
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    if (initialIndex >= totalLength)
+                    {
+                        bitmap.SetPixel(i, j, Color.White);
+                    }
+                    else
+                    {
+                        byte item = completeData[initialIndex];
+                        bitmap.SetPixel(i, j, Color.FromArgb(item, item, item));
+                        initialIndex++;
+
+                        if (initialIndex == totalLength)
+                        {
+                            bitmap.SetPixel(i, j, Color.FromArgb(45, 47, 50));
+                        }
+                    }
+                }
+            }
+
+            return bitmap;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static DecodedFileData ConvertBitmapToFile(Bitmap bitmap)
+    {
+        try
+        {
+            if (bitmap.Width != 512 || bitmap.Height != 512)
+            {
+                return null;
+            }
+
+            byte[] completeData = null;
+            bool finish = false;
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                if (!finish)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        if (!finish)
+                        {
+                            if (bitmap.GetPixel(i, j).R == 45 && bitmap.GetPixel(i, j).G == 47 && bitmap.GetPixel(i, j).B == 50)
+                            {
+                                finish = true;
+                                break;
+                            }
+                            else
+                            {
+                                if (completeData == null)
+                                {
+                                    completeData = new byte[1] { bitmap.GetPixel(i, j).R };
+                                }
+                                else
+                                {
+                                    completeData = Combine(completeData, new byte[1] { bitmap.GetPixel(i, j).R });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            int dataSize = BitConverter.ToInt32(completeData.Take(4).ToArray(), 0);
+            completeData = completeData.Skip(4).ToArray();
+
+            byte[] md5Hash = completeData.Take(16).ToArray();
+            completeData = completeData.Skip(16).ToArray();
+
+            byte isFile = completeData.Take(1).ToArray()[0];
+            completeData = completeData.Skip(1).ToArray();
+
+            if (isFile != 54)
+            {
+                return null;
+            }
+
+            int fileNameSize = BitConverter.ToInt32(completeData.Take(4).ToArray(), 0);
+            completeData = completeData.Skip(4).ToArray();
+
+            string fileName = Encoding.UTF8.GetString(completeData.Take(fileNameSize).ToArray());
+            completeData = completeData.Skip(fileNameSize).ToArray();
+
+            int fileExtensionSize = BitConverter.ToInt32(completeData.Take(4).ToArray(), 0);
+            completeData = completeData.Skip(4).ToArray();
+
+            string fileExtension = Encoding.UTF8.GetString(completeData.Take(fileExtensionSize).ToArray());
+            completeData = completeData.Skip(fileExtensionSize).ToArray();
+
+            if (dataSize != completeData.Length)
+            {
+                return null;
+            }
+
+            if (!CompareByteArrays(md5Hash, GetMD5(completeData)))
+            {
+                return null;
+            }
+
+            return new DecodedFileData()
+            {
+                FileName = fileName,
+                FileExtension = fileExtension,
+                FileData = completeData,
+                FileSize = completeData.Length
+            };
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    private static byte[] Combine(byte[] first, byte[] second)
+    {
+        byte[] ret = new byte[first.Length + second.Length];
+
+        Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+        Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+
+        return ret;
+    }
+
+    private static byte[] GetMD5(byte[] data)
+    {
+        return MD5.Create().ComputeHash(data);
+    }
+
+    private static bool CompareByteArrays(byte[] first, byte[] second)
+    {
+        if (first.Length != second.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < first.Length; i++)
+        {
+            if (first[i] != second[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,272 @@
+﻿using System.Windows.Forms;
+using MetroSuite;
+using System.Drawing;
+using System.Text;
+
+public partial class MainForm : MetroForm
+{
+    private byte[] currentOpenedFile;
+    private string currentOpenedFileName, currentOpenedFileExtension;
+    private DecodedFileData currentDecodedFile;
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void gunaGradientButton3_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            Clipboard.SetText(gunaTextBox1.Text);
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton2_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            Clipboard.SetImage(pictureBox1.BackgroundImage);
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton5_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            pictureBox2.BackgroundImage = Clipboard.GetImage();
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton1_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            saveFileDialog1.FileName = "";
+
+            if (saveFileDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                pictureBox1.BackgroundImage.Save(saveFileDialog1.FileName);
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton6_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            openFileDialog1.FileName = "";
+
+            if (openFileDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                pictureBox2.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton4_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            Clipboard.SetText(gunaTextBox2.Text);
+
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton17_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            pictureBox1.BackgroundImage = ColorizedImageTranslator.StringConvertBytesToBitmap(Encoding.UTF8.GetBytes(gunaTextBox1.Text), 512, 512);
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton7_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            byte[] bytes = ColorizedImageTranslator.StringConvertBitmapToBytes((Bitmap)pictureBox2.BackgroundImage);
+            gunaTextBox2.Text = Encoding.UTF8.GetString(bytes);
+            metroLabel2.Text = "Informations written: " + bytes.Length.ToString() + "/" + gunaTextBox2.MaxLength;
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaTextBox1_TextChanged(object sender, System.EventArgs e)
+    {
+        try
+        {
+            metroLabel1.Text = "Informations written: " + Encoding.UTF8.GetByteCount(gunaTextBox1.Text) + "/" + gunaTextBox1.MaxLength;
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton10_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            saveFileDialog1.FileName = "";
+
+            if (saveFileDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                pictureBox3.BackgroundImage.Save(saveFileDialog1.FileName);
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton11_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            Clipboard.SetImage(pictureBox3.BackgroundImage);
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton8_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            openFileDialog2.FileName = "";
+
+            if (openFileDialog2.ShowDialog().Equals(DialogResult.OK))
+            {
+                byte[] fileBytes = System.IO.File.ReadAllBytes(openFileDialog2.FileName);
+
+                if (fileBytes.Length > 260000)
+                {
+                    return;
+                }
+
+                currentOpenedFile = fileBytes;
+                currentOpenedFileExtension = System.IO.Path.GetExtension(openFileDialog2.FileName).Substring(1);
+                currentOpenedFileName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog2.FileName);
+
+                metroLabel3.Text = "File bytes: " + fileBytes.Length + "/260000\r\n" +
+                    "File name: " + currentOpenedFileName + "\r\n" +
+                    "File extension: " + currentOpenedFileExtension + "\r\n" +
+                    "File name with extension: " + currentOpenedFileName + "." + currentOpenedFileExtension;
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton9_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            pictureBox3.BackgroundImage = ColorizedImageTranslator.FileConvertBytesToBitmap(currentOpenedFile, currentOpenedFileName, currentOpenedFileExtension, 512, 512);
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton12_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            openFileDialog1.FileName = "";
+
+            if (openFileDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                pictureBox4.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton13_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            pictureBox4.BackgroundImage = Clipboard.GetImage();
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton14_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            currentDecodedFile = ColorizedImageTranslator.ConvertBitmapToFile((Bitmap)pictureBox4.BackgroundImage);
+            metroLabel4.Text = "File bytes: " + currentDecodedFile.FileSize + "/260000\r\n" +
+                "File name: " + currentDecodedFile.FileName + "\r\n" +
+                "File extension: " + currentDecodedFile.FileExtension + "\r\n" +
+                "File name with extension: " + currentDecodedFile.FileName + "." + currentDecodedFile.FileExtension;
+        }
+        catch
+        {
+
+        }
+    }
+
+    private void gunaGradientButton15_Click(object sender, System.EventArgs e)
+    {
+        try
+        {
+            saveFileDialog2.Filter = currentDecodedFile.FileExtension.ToUpper() + " file (*." + currentDecodedFile.FileExtension + ")|*." + currentDecodedFile.FileExtension;
+            saveFileDialog2.FileName = currentDecodedFile.FileName;
+
+            if (saveFileDialog2.ShowDialog().Equals(DialogResult.OK))
+            {
+                System.IO.File.WriteAllBytes(saveFileDialog2.FileName, currentDecodedFile.FileData);
+            }
+        }
+        catch
+        {
+
+        }
+    }
+}
